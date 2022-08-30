29eb06d17a747489578a3a7bc80e013c291b2918
Note <57417232+Rabbitnote@users.noreply.github.com>
2022-08-30 18:07:10 +0700

    [SYN-92] Finish Function login and Page Verification (#2)
    * [UPDATE] Finish Function login and Page Verification
    
    * [UPDATE] Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "./Page/Login";
+import OtpVerificationPage from "./Page/Login/otpVerificationPage";
+import VerifiedSuccesPage from "./Page/Login/VerifiedSuccesPage";
+import HomePage from "./Page/home";
 
 function App() {
   // const Login = lazy(() => import("./Page/Login"));
@@ -8,6 +11,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/otp-verification" element={<OtpVerificationPage />} />
+        <Route path="/verified-success" element={<VerifiedSuccesPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.synthia.tech/patient/api",
+  withCredentials: false,
+});
+
+export default api;
