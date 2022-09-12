c5c1ca5a263f0f7d2ec9a7ec96731365e63badf9
note <note16651@hotmail.co.th>
2022-09-12 12:29:32 +0700

    [UPDATE] State Management

--- src/Page/home.js
+++ src/Page/home.js
@@ -1,10 +1,18 @@
-const HomePage = () => {
+import { connect } from "react-redux";
+
+const mapState = (state) => ({
+  user: state.user,
+});
+
+const HomePage = (props) => {
+  console.log(props.name);
   return (
     <div className="">
       <h1 className="typographyHeadingMdSemibold text-primary-600">
         Hello this is home page
       </h1>
+      <h1>{props.user.credential}</h1>
     </div>
   );
 };
-export default HomePage;
+export default connect(mapState)(HomePage);
