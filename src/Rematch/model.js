19a4ffaa427ee7602b8909bb5e91c4477e44053b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-09-30 23:08:18 +0700

    use jwt from localstorage

--- src/Rematch/model.js
+++ src/Rematch/model.js
@@ -1,28 +1,28 @@
 //EXAMPLE Rematch
 export const count = {
-  state: 0, // initial state
-  reducers: {
-    // handle state changes with pure functions
-    increment(state, payload) {
-      return state + payload;
-    },
-  },
-  effects: (dispatch) => ({
-    // handle state changes with impure functions.
-    // use async/await for async actions
-    async incrementAsync(payload, rootState) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      dispatch.count.increment(payload);
-    },
-  }),
-};
+	state: 0, // initial state
+	reducers: {
+		// handle state changes with pure functions
+		increment(state, payload) {
+			return state + payload
+		}
+	},
+	effects: (dispatch) => ({
+		// handle state changes with impure functions.
+		// use async/await for async actions
+		async incrementAsync(payload, rootState) {
+			await new Promise((resolve) => setTimeout(resolve, 1000))
+			dispatch.count.increment(payload)
+		}
+	})
+}
 
 export const user = {
-  state: {
-    tokenJWT: "",
-  },
-  reducers: {
-    setName: (state, payload) => payload,
-    setToken: (state, payload) => payload,
-  },
-};
+	state: {
+		tokenJWT: localStorage.getItem("jwt") || ""
+	},
+	reducers: {
+		setName: (state, payload) => payload,
+		setToken: (state, payload) => payload
+	}
+}
