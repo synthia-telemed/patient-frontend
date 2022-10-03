59dc77583424c9fb5bb51ea412bcb4980b625ed9
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-03 15:15:24 +0700

    add: useAPI

---
+++ src/hooks/useAPI.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const useAPI = () => {
+  const { tokenJWT } = useSelector(state => state.user);
+  const instance = axios.create({
+    baseURL: "https://api.synthia.tech/patient/api",
+    withCredentials: false,
+    headers: {
+      Authorization: "Bearer " + tokenJWT
+    }
+  });
+  return [instance];
+};
+
+export default useAPI;
