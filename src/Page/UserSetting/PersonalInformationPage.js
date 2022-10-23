95432630cda75dce4f7fca4ecc8823cd0429d7a4
Chingly <57487560+KavisaraSrisuwatcharee@users.noreply.github.com>
2022-10-23 12:00:09 +0700

    [FIX] typo national id number

--- src/Page/UserSetting/PersonalInformationPage.js
+++ src/Page/UserSetting/PersonalInformationPage.js
@@ -7,7 +7,7 @@ const PersonalInformationPage = () => {
       <PersonalInformationCard title="Name" value="Kavisara Srisuwatcharee" />
       <PersonalInformationCard title="Birthdate" value="20/06/2000" />
       <PersonalInformationCard title="Patient Number" value="HN-103191" />
-      <PersonalInformationCard title="Citizen Number" value="1-1675-08162-0" />
+      <PersonalInformationCard title="National ID Number" value="1-1675-08162-0" />
       <PersonalInformationCard title="Nationality" value="Thai" />
       <PersonalInformationCard title="Weight" value="45 Kg." />
       <PersonalInformationCard title="Height" value="159 cm." />
