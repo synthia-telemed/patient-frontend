05b72d6bf4eebb712d4e1740861a73ff472445ab
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-31 22:21:01 +0700

    add: read notification when tap

--- src/Page/index.js
+++ src/Page/index.js
@@ -4,15 +4,25 @@ import { useNavigate } from "react-router-dom";
 import { Preferences } from "@capacitor/preferences";
 import { PushNotifications } from "@capacitor/push-notifications";
 import { Capacitor } from "@capacitor/core";
+import useAPI from "../hooks/useAPI";
 
 const IndexPage = () => {
+  const [api] = useAPI();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const checkUserLogin = async () => {
+  const loadSavedToken = async () => {
     const token = await Preferences.get({ key: "token" });
     if (token.value) {
       dispatch.user.setToken(token.value);
+      return token.value;
+    }
+    return null;
+  };
+
+  const checkUserLogin = async () => {
+    const token = await loadSavedToken();
+    if (token) {
       if (Capacitor.isNativePlatform()) await registerPushnotificationListener();
       return navigate(`/home`);
     }
@@ -26,8 +36,21 @@ const IndexPage = () => {
 
     await PushNotifications.addListener(
       "pushNotificationActionPerformed",
-      ({ notification }) => {
-        const { appointmentID } = notification.data;
+      async ({ notification }) => {
+        const { appointmentID, notificationID } = notification.data;
+        const token = await loadSavedToken();
+        if (!token) return navigate("/login");
+        try {
+          await api.patch(`/notification/${notificationID}`, null, {
+            headers: { Authorization: `Bearer ${token}` }
+          });
+        } catch (error) {
+          console.error(
+            "read notification error",
+            error.response.status,
+            JSON.stringify(error.response.data)
+          );
+        }
         if (appointmentID) {
           return navigate("/appointment/detail", { state: { appointmentID } });
         }
