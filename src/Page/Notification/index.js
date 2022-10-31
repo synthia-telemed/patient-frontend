b7afc0c7dfbc8f4f0767c9f0f352865913a24100
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-31 23:08:26 +0700

    Feat/wired up notification (#37)
    * add: route
    
    * fetch notification from api
    
    * add: set all as read when read all
    
    * fix: wrong boolean

--- src/Page/Notification/index.js
+++ src/Page/Notification/index.js
@@ -2,31 +2,40 @@ import NotificationHeader from "../../components/Notification/notificationHeader
 import DatepickerIcon from "../../Assets/NotificationIcon/datepicker.svg";
 import WalletIcon from "../../Assets/NotificationIcon/wallet.svg";
 import NotificationCard from "../../components/Notification/notificationCard";
+import useAPI from "../../hooks/useAPI";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
 const NotificationPage = () => {
+  const [notifications, setNotification] = useState([]);
+  const [api] = useAPI();
+
+  useEffect(() => {
+    api.get("/notification").then(({ data }) => {
+      setNotification(data);
+    });
+  }, []);
+
+  const onSetAllAsRead = () => {
+    setNotification(notifications.map(noti => ({ ...noti, is_read: true })));
+  };
+
   return (
     <div>
-      <NotificationHeader />
-      <NotificationCard
-        title="Appointment success!"
-        url={DatepickerIcon}
-        statusNew={true}
-        time="Today 14:30 PM"
-        detail="You have an appointment with Dr. Alice Wonderland on December 24 , 2022 , 10:00 am. Don’t forget to activate your remember."
-      />
-      <NotificationCard
-        title="You recieve a new payment"
-        url={WalletIcon}
-        statusNew={false}
-        time="Today 14:30 PM"
-        detail="You recieve a new payment. Please payment before 24 hours. "
-      />
-      <NotificationCard
-        title="Schedule changed"
-        url={DatepickerIcon}
-        statusNew={false}
-        time="21 Aug 2022  14:30 PM"
-        detail="The schedule appointment is changed from hospital. You have an  appointment with Dr. Alice Wonderland on December 24 , 2022 , 10:00 am. Don’t forget to activate your remember."
-      />
+      <NotificationHeader onSetAllAsRead={onSetAllAsRead} />
+      {notifications.map(noti => {
+        return (
+          <NotificationCard
+            title={noti.title}
+            url={DatepickerIcon}
+            statusNew={!noti.is_read}
+            time={dayjs(noti.created_at).fromNow()}
+            detail={noti.body}
+          />
+        );
+      })}
     </div>
   );
 };
