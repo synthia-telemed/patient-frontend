9e8232e37bd9b528384a21039735c993082555d4
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-11-02 13:39:22 +0700

    change glucose to blood pressure

--- src/Page/Report/index.js
+++ src/Page/Report/index.js
@@ -21,7 +21,7 @@ import {
 
 const ReportPage = () => {
   const [showModal, setShowModal] = useState(false);
-  const [glucoseData, setGlucoseData] = useState({});
+  const [bloodPressureData, setBloodPressureData] = useState([]);
   const [apiDefault] = useApiMeasurement();
   const current = new Date();
   const barColors = ["#1f77b4", "#ff7f0e", "#2ca02c"];
@@ -63,168 +63,174 @@ const ReportPage = () => {
   const onClickCloseModal = () => {
     setShowModal(false);
   };
-  const fetchGlucoseValue = async () => {
-    const query = { granularity: "day", date: current.toISOString() };
+  const fetchBloodPressure = async () => {
+    const query = { granularity: "month", date: current.toISOString() };
     console.log(current.toISOString());
     const res = await apiDefault.get("/blood-pressure/visualization/patient", {
       params: query
     });
-    setGlucoseData(res.data);
+    setBloodPressureData(res.data);
   };
 
   useEffect(() => {
-    fetchGlucoseValue();
+    fetchBloodPressure();
   }, []);
   useEffect(() => {
-    fetchGlucoseValue();
+    fetchBloodPressure();
   }, [showModal]);
-  console.log(glucoseData);
 
   return (
     <Layout>
-    <div className="overflow-auto h-[1000px]">
-      <HeaderReport />
-      <Navbar />
-      <FloatingButton onClick={onClickReportMeasure} />
-      <div className="px-[16px] mt-[16px]">
-        <h1 className="typographyTextXlSemibold">Blood Pressure</h1>
-        <h1 className="typographyTextSmMedium text-gray-600">Total avg this day</h1>
-        <div className="flex items-center mt-[5px]">
-          <h1 className="typographyHeadingXsSemibold text-success-700 mr-[16px] mb-[19px]">
-            120/80 <span className="typographyTextSmMedium text-gray-600">mmHg</span>
+      <div className="overflow-auto h-[1000px]">
+        <HeaderReport />
+        <Navbar />
+        <FloatingButton onClick={onClickReportMeasure} />
+        <div className="px-[16px] mt-[16px]">
+          <h1 className="typographyTextXlSemibold">Blood Pressure</h1>
+          <h1 className="typographyTextSmMedium text-gray-600">Total avg this day</h1>
+          <div className="flex items-center mt-[5px]">
+            <h1 className="typographyHeadingXsSemibold text-success-700 mr-[16px] mb-[19px]">
+              120/80 <span className="typographyTextSmMedium text-gray-600">mmHg</span>
+            </h1>
+            <BadgeStatus text="Normal" style="bg-success-50 text-success-700" />
+          </div>
+          <ResponsiveContainer width="100%" height={240} className="ml-[-16px]">
+            <BarChart
+              width={830}
+              height={250}
+              data={bloodPressureData.data}
+              className="mt-[5px]"
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="label"
+                // label={glucoseData.xLabel}
+                ticks={bloodPressureData.isNumerical ? [0, 360, 720, 1080, 1440] : null}
+                axisLine={false}
+                className="typographyTextXsMedium"
+                tickFormatter={
+                  bloodPressureData.isNumerical ? t => `${Math.floor(t / 60)}:00` : t => t
+                }
+                type={bloodPressureData.isNumerical ? "number" : "category"}
+              />
+              <YAxis
+                domain={[0, 200]}
+                axisLine={false}
+                className="typographyTextXsMedium"
+              />
+              <Tooltip />
+              <Bar barSize={10} dataKey="values" fill="fill" radius={30}>
+                <LabelList
+                  className="typographyTextXsMedium"
+                  width={20}
+                  dataKey="values"
+                  formatter={v => `${v[1]} ${bloodPressureData.unit}`}
+                  position="top"
+                />
+                <LabelList
+                  className="typographyTextXsMedium"
+                  width={20}
+                  dataKey="values"
+                  formatter={v => `${v[0]} ${bloodPressureData.unit}`}
+                  position="bottom"
+                />
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+          <h1 className="typographyTextXsMedium text-gray-500 text-center">
+            {bloodPressureData.xLabel}
           </h1>
-          <BadgeStatus text="Normal" style="bg-success-50 text-success-700" />
         </div>
-        <ResponsiveContainer width="100%" height={240} className="ml-[-16px]">
-          <BarChart width={830} height={250} data={glucoseData.data} className="mt-[5px]">
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="label"
-              // label={glucoseData.xLabel}
-              // ticks={ticks}
-              axisLine={false}
-              className="typographyTextXsMedium"
-              tickFormatter={
-                glucoseData.isNumerical ? t => `${Math.floor(t / 60)}:00` : t => t
-              }
-              type={glucoseData.isNumerical ? "number" : "category"}
-            />
-            <YAxis
-              domain={[0, 200]}
-              axisLine={false}
-              className="typographyTextXsMedium"
-            />
-            <Tooltip />
-            <Bar barSize={10} dataKey="values" fill="fill" radius={30}>
-              <LabelList
-                className="typographyTextXsMedium"
-                width={20}
-                dataKey="values"
-                formatter={v => `${v[1]} ${glucoseData.unit}`}
-                position="top"
-              />
-              <LabelList
-                className="typographyTextXsMedium"
-                width={20}
-                dataKey="values"
-                formatter={v => `${v[0]} ${glucoseData.unit}`}
-                position="bottom"
-              />
-            </Bar>
-          </BarChart>
-        </ResponsiveContainer>
-        <h1 className="typographyTextXsMedium text-gray-500 text-center">
-          {glucoseData.xLabel}
-        </h1>
-      </div>
 
-      <div className="px-[16px] mt-[16px]">
-        <h1 className="typographyTextXlSemibold">Glucose Level</h1>
-        <h1 className="typographyTextSmMedium text-gray-600">Total avg this day</h1>
-        <div className="flex items-center mt-[5px]">
-          <h1 className="typographyHeadingXsSemibold text-success-700 mr-[16px] mb-[19px]">
-            130
-            <span className="typographyTextSmMedium text-gray-600 ml-[4px]">mg/dL</span>
-          </h1>
-          <BadgeStatus text="Normal" style="bg-success-50 text-success-700" />
-        </div>
-        <ResponsiveContainer width="100%" height={240} className="ml-[-16px] ">
-          <BarChart
-            width={830}
-            height={250}
-            data={data2}
-            className="mt-[5px] z-1 relative"
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="label"
-              label={x_label2}
-              ticks={ticks2}
-              axisLine={false}
-              className="typographyTextXsMedium"
-              tickFormatter={isNumerical2 ? t => `${Math.floor(t / 60)}:00` : t => t}
-              type={isNumerical2 ? "number" : "category"}
-            />
-            <YAxis
-              domain={[0, 200]}
-              axisLine={false}
-              className="typographyTextXsMedium"
-            />
-            <Tooltip wrapperStyle={{ zIndex: 1000 }} />
-            <Bar barSize={10} dataKey="values" fill="#8884d8" radius={30}>
-              <LabelList
+        <div className="px-[16px] mt-[16px]">
+          <h1 className="typographyTextXlSemibold">Glucose Level</h1>
+          <h1 className="typographyTextSmMedium text-gray-600">Total avg this day</h1>
+          <div className="flex items-center mt-[5px]">
+            <h1 className="typographyHeadingXsSemibold text-success-700 mr-[16px] mb-[19px]">
+              130
+              <span className="typographyTextSmMedium text-gray-600 ml-[4px]">mg/dL</span>
+            </h1>
+            <BadgeStatus text="Normal" style="bg-success-50 text-success-700" />
+          </div>
+          <ResponsiveContainer width="100%" height={240} className="ml-[-16px] ">
+            <BarChart
+              width={830}
+              height={250}
+              data={data2}
+              className="mt-[5px] z-1 relative"
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="label"
+                label={x_label2}
+                ticks={ticks2}
+                axisLine={false}
                 className="typographyTextXsMedium"
-                width={20}
-                dataKey="values"
-                formatter={v => `${v[0]} ${unit2}`}
-                position="top"
+                tickFormatter={isNumerical2 ? t => `${Math.floor(t / 60)}:00` : t => t}
+                type={isNumerical2 ? "number" : "category"}
               />
-            </Bar>
-          </BarChart>
-        </ResponsiveContainer>
-        <h1 className="typographyTextXsMedium text-gray-500 text-center">days</h1>
-      </div>
-      {showModal ? (
-        <>
-          {" "}
-          <div className="fixed inset-0 z-50 outline-none focus:outline-none overflow-auto">
-            <div className="absolute bottom-[0%] w-auto mx-auto max-w-3xl ">
-              {/*content*/}
-              <div className="bg-base-white h-[600px] w-screen px-[16px] py-[24px] rounded-tl-[20px] rounded-tr-[20px]">
-                <div className="flex justify-between">
-                  <h1 className="typographyTextLgSemibold">Report Measurement Result</h1>
-                  <span
-                    className="w-[24px] h-[24px] text-gray-600 text-[24px] flex justify-center items-center"
-                    onClick={() => setShowModal(false)}
-                  >
-                    ×
-                  </span>
-                </div>
-                <div className="mt-[12px] ">
-                  <div className="flex items-center ">
-                    <h1 className="typographyTextSmMedium">Date & Time</h1>
-                    <div className="ml-[5px]">
-                      <DateTimePicker
-                        value={selectedDate}
-                        onChange={handleDateChange}
-                        inputVariant="outlined"
-                      />
-                    </div>
+              <YAxis
+                domain={[0, 200]}
+                axisLine={false}
+                className="typographyTextXsMedium"
+              />
+              <Tooltip wrapperStyle={{ zIndex: 1000 }} />
+              <Bar barSize={10} dataKey="values" fill="#8884d8" radius={30}>
+                <LabelList
+                  className="typographyTextXsMedium"
+                  width={20}
+                  dataKey="values"
+                  formatter={v => `${v[0]} ${unit2}`}
+                  position="top"
+                />
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+          <h1 className="typographyTextXsMedium text-gray-500 text-center">days</h1>
+        </div>
+        {showModal ? (
+          <>
+            {" "}
+            <div className="fixed inset-0 z-50 outline-none focus:outline-none overflow-auto">
+              <div className="absolute bottom-[0%] w-auto mx-auto max-w-3xl ">
+                {/*content*/}
+                <div className="bg-base-white h-[600px] w-screen px-[16px] py-[24px] rounded-tl-[20px] rounded-tr-[20px]">
+                  <div className="flex justify-between">
+                    <h1 className="typographyTextLgSemibold">
+                      Report Measurement Result
+                    </h1>
+                    <span
+                      className="w-[24px] h-[24px] text-gray-600 text-[24px] flex justify-center items-center"
+                      onClick={() => setShowModal(false)}
+                    >
+                      ×
+                    </span>
                   </div>
-                  <ReportTab
-                    selectedDate={selectedDate}
-                    setShowModal={onClickCloseModal}
-                  />
+                  <div className="mt-[12px] ">
+                    <div className="flex items-center ">
+                      <h1 className="typographyTextSmMedium">Date & Time</h1>
+                      <div className="ml-[5px]">
+                        <DateTimePicker
+                          value={selectedDate}
+                          onChange={handleDateChange}
+                          inputVariant="outlined"
+                        />
+                      </div>
+                    </div>
+                    <ReportTab
+                      selectedDate={selectedDate}
+                      setShowModal={onClickCloseModal}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="opacity-50 fixed inset-0 z-40 bg-base-white"></div>
-        </>
-      ) : (
-        <></>
-      )}
-    </div>
+            <div className="opacity-50 fixed inset-0 z-40 bg-base-white"></div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </Layout>
   );
 };
