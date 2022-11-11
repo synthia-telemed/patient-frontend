f9a90da6df8be1e991031d59a5b70fff4f17763a
note <note16651@hotmail.co.th>
2022-11-11 17:43:30 +0700

    [UPDATE] Condition Empty Graph

--- src/Page/Report/MonthTab.js
+++ src/Page/Report/MonthTab.js
@@ -1,23 +1,40 @@
 import GraphBarReport from "../../components/Report/GraphBarReport";
 import GraphLineReport from "../../components/Report/GraphLineReport";
+import EmptyGraph from "../../Assets/Report/empty_graph.svg";
 const MonthTab = ({ bloodPressureData }) => {
   return (
     <div className="px-[16px] mt-[16px] mb-[100px]">
       {/* <GraphLineReport data={bloodPressureData} name="Glucose" /> */}
-      <GraphBarReport
-        data={bloodPressureData}
-        name="Glucose"
-        isLegend={false}
-        detailGraph="Total avg this day"
-        isHaveLastLabelList={true}
-        panel="Month"
-        summaryValue={
-          parseFloat(bloodPressureData?.summary?.systolic).toFixed(2) +
-          " / " +
-          parseFloat(bloodPressureData?.summary?.diastolic).toFixed(2) +
-          " "
-        }
-      />
+      {bloodPressureData?.data?.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-[50vh]">
+          {" "}
+          <img src={EmptyGraph} width="218px" height="148px" alt="" />{" "}
+          <h1 className="typographyTextXsMedium text-gray-800">
+            {" "}
+            You haven’t submited any measurement result.
+          </h1>
+        </div>
+      ) : (
+        <></>
+      )}
+      {bloodPressureData?.data?.length === 0 ? (
+        <></>
+      ) : (
+        <GraphBarReport
+          data={bloodPressureData}
+          name="Glucose"
+          isLegend={false}
+          detailGraph="Total avg this day"
+          isHaveLastLabelList={true}
+          panel="Month"
+          summaryValue={
+            parseFloat(bloodPressureData?.summary?.systolic).toFixed(2) +
+            " / " +
+            parseFloat(bloodPressureData?.summary?.diastolic).toFixed(2) +
+            " "
+          }
+        />
+      )}
       {/* <GraphBarReport data={bloodPressureData} name="Pulse" /> */}
     </div>
   );
