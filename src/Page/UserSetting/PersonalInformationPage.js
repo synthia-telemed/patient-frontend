0c230e1d795b105908ec39f47698f88deaa6aad5
note <note16651@hotmail.co.th>
2022-11-17 13:31:09 +0700

    [UPDATE] Information User

--- src/Page/UserSetting/PersonalInformationPage.js
+++ src/Page/UserSetting/PersonalInformationPage.js
@@ -1,17 +1,42 @@
+import { useEffect, useState } from "react";
 import HeaderWithBack from "../../components/HeaderWithBack";
+import useAPI from "../../hooks/useAPI";
 import PersonalInformationCard from "../../components/SettingPanel/PersonalInformationCard";
+import dayjs from "dayjs";
 
 const PersonalInformationPage = () => {
+  const [informationUser, setInformationUser] = useState([]);
+  const [apiDefault] = useAPI();
+
+  useEffect(() => {
+    fetchInformationUser();
+  }, []);
+  const fetchInformationUser = async () => {
+    const res = await apiDefault.get("/info");
+    setInformationUser(res.data);
+  };
+
   return (
     <div>
       <HeaderWithBack textHeader="Personal Information" path="/setting" />
-      <PersonalInformationCard title="Name" value="Kavisara Srisuwatcharee" />
-      <PersonalInformationCard title="Birthdate" value="20/06/2000" />
-      <PersonalInformationCard title="Patient Number" value="HN-103191" />
-      <PersonalInformationCard title="National ID Number" value="1-1675-08162-0" />
-      <PersonalInformationCard title="Nationality" value="Thai" />
-      <PersonalInformationCard title="Weight" value="45 Kg." />
-      <PersonalInformationCard title="Height" value="159 cm." />
+      <PersonalInformationCard
+        title="Name"
+        value={
+          informationUser?.name_en?.firstname + " " + informationUser?.name_en?.lastname
+        }
+      />
+      <PersonalInformationCard
+        title="Birthdate"
+        value={dayjs(informationUser?.birth_date).format("YYYY-MM-DD")}
+      />
+      <PersonalInformationCard title="Patient Number" value="-" />
+      <PersonalInformationCard
+        title="National ID Number"
+        value={informationUser?.national_id}
+      />
+      <PersonalInformationCard title="Nationality" value={informationUser?.nationality} />
+      <PersonalInformationCard title="Weight" value={informationUser?.weight} />
+      <PersonalInformationCard title="Height" value={informationUser?.height} />
       <PersonalInformationCard title="Blood Type" value="A" />
     </div>
   );
--- src/Page/UserSetting/index.js
+++ src/Page/UserSetting/index.js
@@ -5,6 +5,8 @@ import PeopleIcon from "../../Assets/UserSetting/peopleIcon.svg";
 import CreditcardIcon from "../../Assets/UserSetting/creditcard.svg";
 import AgreementIcon from "../../Assets/UserSetting/agreement.svg";
 import PrivacyIcon from "../../Assets/UserSetting/privacy.svg";
+import useAPI from "../../hooks/useAPI";
+import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -12,20 +14,29 @@ import { useDispatch } from "react-redux";
 const UserSettingPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [apiDefault] = useAPI();
+  const [informationUser, setInformationUser] = useState([]);
   const onLogout = () => {
     Preferences.remove({ key: "token" }).then(() => {
       dispatch.user.removeToken();
       navigate("/login");
     });
   };
+  useEffect(() => {
+    fetchInformationUser();
+  }, []);
+  const fetchInformationUser = async () => {
+    const res = await apiDefault.get("/info");
+    setInformationUser(res.data);
+  };
   return (
     <div>
       <div className="mt-[71px]">
         <div className="px-[16px] h-[80vh]">
           <div className="flex h-[75px]">
-            <img src={ProfileUserIcon} alt="" />
+            <img src={informationUser?.profile_pic_url} className="rounded-[48px]" alt="" />
             <div className="flex flex-col ml-[16px] justify-center">
-              <h1 className="typographyTextMdSemibold">Sansa Stark</h1>
+              <h1 className="typographyTextMdSemibold">{informationUser?.name_en?.firstname+" " +informationUser?.name_en?.lastname}</h1>
               <h2 className="typographyTextXsMedium text-gray-700">
                 Siriraj Piyamaharajkarun Hospital
               </h2>
